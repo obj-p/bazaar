21f8dec6487f9ebbe56b2f065c49746ddd8214b7
Jason Prasad <jasongprasad@gmail.com>
2025-06-21 23:52:50 -0400

    Add listener

--- Tests/TemplatesTests/TemplatesParserTests.swift
+++ Tests/TemplatesTests/TemplatesParserTests.swift
@@ -3,17 +3,34 @@ import Templates
 import Testing
 
 struct TemplatesParserTests {
+    static let example = """
+    data Text(
+        value String
+    )
+    """
+
     @Test
     func parser() throws {
         // Given
-        let input = ANTLRInputStream("")
+        let input = ANTLRInputStream(Self.example)
         let lexer = TemplatesLexer(input)
-        let ts = BufferedTokenStream(lexer)
+        let ts = CommonTokenStream(lexer)
+        let parser = try TemplatesParser(ts)
+        let tree = try parser.schema()
 
         // When
-        let parser = try TemplatesParser(ts)
+        Visitor().visit(tree)
+    }
+}
 
-        // Then
-        print(parser)
+class Visitor: TemplatesParserBaseVisitor<Void> {
+    override func visitData(_ ctx: TemplatesParser.DataContext) {
+        print(ctx.getText())
+
+        super.visitData(ctx)
+    }
+
+    override func visitMember(_ ctx: TemplatesParser.MemberContext) {
+        print(ctx.getText())
     }
 }
