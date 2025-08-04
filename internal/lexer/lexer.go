269d62af65c9e8337d34a77d05ead080ddefc4ca
Jason Prasad <jasongprasad@gmail.com>
2025-08-04 18:10:47 -0400

    Update lexer to parse Int and Float

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -4,7 +4,8 @@ import "github.com/alecthomas/participle/v2/lexer"
 
 var BazaarLexer = lexer.MustSimple([]lexer.SimpleRule{
 	{Name: "Comment", Pattern: `//.*`},
-	{Name: "Number", Pattern: `(?:\d*\.)?\d+`},
+	{Name: "Float", Pattern: `\d*\.\d+`},
+	{Name: "Int", Pattern: `\d+`},
 	{Name: "String", Pattern: `"(\\"|[^"])*"`},
 	{Name: "Ident", Pattern: `[\w_][\w\d_]*`},
 	{Name: "Operator", Pattern: `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
