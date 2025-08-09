4930ba67d4e24d23a5de06a3d9db26f808bb39f1
Jason Prasad <jasongprasad@gmail.com>
2025-08-09 14:09:01 -0400

    Add String expression parsing

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -2,12 +2,27 @@ package lexer
 
 import "github.com/alecthomas/participle/v2/lexer"
 
-var BazaarLexer = lexer.MustSimple([]lexer.SimpleRule{
-	{Name: "Comment", Pattern: `//.*`},
-	{Name: "Float", Pattern: `\d*\.\d+`},
-	{Name: "Int", Pattern: `\d+`},
-	{Name: "String", Pattern: `"(\\"|[^"])*"`},
-	{Name: "Ident", Pattern: `[\w_][\w\d_]*`},
-	{Name: "Operator", Pattern: `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
-	{Name: "Whitespace", Pattern: `[ \t\n\r]+`},
+var BazaarLexer = lexer.MustStateful(lexer.Rules{
+	"Root": {
+		lexer.Include("Tokens"),
+		{Name: "String", Pattern: `"`, Action: lexer.Push("String")},
+	},
+	"String": {
+		{Name: "Escaped", Pattern: `\\.`, Action: nil},
+		{Name: "StringEnd", Pattern: `"`, Action: lexer.Pop()},
+		{Name: "StringExpr", Pattern: `\${`, Action: lexer.Push("StringExpr")},
+		{Name: "Char", Pattern: `\$|[^$"\\]+`, Action: nil},
+	},
+	"StringExpr": {
+		{Name: "StringExprEnd", Pattern: `}`, Action: lexer.Pop()},
+		lexer.Include("Tokens"),
+	},
+	"Tokens": {
+		{Name: "Comment", Pattern: `//.*`, Action: nil},
+		{Name: "Float", Pattern: `\d*\.\d+`, Action: nil},
+		{Name: "Int", Pattern: `\d+`, Action: nil},
+		{Name: "Ident", Pattern: `[\w_][\w\d_]*`, Action: nil},
+		{Name: "Operator", Pattern: `[-[!@#$%^&*()+_={}\|:;'<,>.?/]|]`, Action: nil},
+		{Name: "Whitespace", Pattern: `[ \t\n\r]+`, Action: nil},
+	},
 })
