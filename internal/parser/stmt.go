b5591901128fce8e6bf1f27fa60aa478d3ca01c7
Jason Prasad <jasongprasad@gmail.com>
2025-08-30 09:27:11 -0400

    WIP IfStmt

--- internal/parser/stmt.go
+++ internal/parser/stmt.go
@@ -1,5 +1,16 @@
 package parser
 
+type AssignStmt struct {
+	Name *string `parser:"'var' @Ident '='"`
+	Expr *Expr   `parser:"@@"`
+}
+
+type IfStmt struct {
+	Assign *AssignStmt `parser:"'if' (@@"`
+	Expr   *Expr       `parser:"| @@)"`
+	Block  *Block      `parser:"@@"`
+}
+
 type ForStmt struct {
 	Key    *string `parser:"'for' @Ident"`
 	Source *Expr   `parser:"'in' @@"`
@@ -8,6 +19,7 @@ type ForStmt struct {
 
 type Stmt struct {
 	For  *ForStmt `parser:"@@"`
+	If   *IfStmt  `parser:"| @@"`
 	Expr *Expr    `parser:"| @@"`
 }
 
